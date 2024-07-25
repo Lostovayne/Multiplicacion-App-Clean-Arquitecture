245a822679994c66ec61e0b461ba6c7f5ae1480d
LostoVayne <moises.barrera.dev@gmail.com>
2024-07-24 20:25:16 -0400

    agregadas las pruebas de testing de los spy

--- src/tests/domain/use-cases/save-files.use-case.test.ts
+++ src/tests/domain/use-cases/save-files.use-case.test.ts
@@ -1,7 +1,13 @@
 import { SaveFile } from "../../../domain/use-cases/save-file.use-case";
-import fs from "fs";
+import fs, { writeFileSync } from "fs";
 
 describe("SaveFileUseCase", () => {
+  const Customoptions = {
+    fileContent: "custom content",
+    fileDestination: "custom-outputs/file-destination",
+    fileName: "custom-table-name",
+  };
+
   afterEach(() => {
     if (fs.existsSync("outputs/table.txt")) {
       fs.unlinkSync("outputs/table.txt");
@@ -11,17 +17,15 @@ describe("SaveFileUseCase", () => {
   test("should save file with default values", () => {
     const saveFile = new SaveFile();
     const filePath = "outputs/table.txt";
-
     const options = {
       fileContent: "Hello World",
     };
 
     const result = saveFile.execute(options);
-
-    expect(result).toBe(true);
-
     const checkfile = fs.existsSync(filePath); // Checkear el archivo
     const fileContent = fs.readFileSync(filePath, "utf-8");
+
+    expect(result).toBe(true);
     expect(fileContent).toBe(options.fileContent);
     expect(checkfile).toBe(true);
   });
@@ -36,22 +40,42 @@ describe("SaveFileUseCase", () => {
 
   test("should save file with custom values", () => {
     const saveFile = new SaveFile();
-    const options = {
-      fileContent: "custom content",
-      fileDestination: "custom-outputs/file-destination",
-      fileName: "custom-table-name",
-    };
+    const CustomFilePath =
+      "custom-outputs/file-destination/custom-table-name.txt";
 
-    const result = saveFile.execute(options);
-    const fileContent = fs.readFileSync(
-      `${options.fileDestination}/${options.fileName}.txt`,
-      "utf-8"
-    );
-
-    const ExistFile = fs.existsSync(options.fileDestination);
+    const result = saveFile.execute(Customoptions);
+    const fileContent = fs.readFileSync(CustomFilePath, "utf-8");
+    const ExistFile = fs.existsSync(Customoptions.fileDestination);
 
     expect(ExistFile).toBe(true);
     expect(result).toBe(true);
-    expect(fileContent).toBe(options.fileContent);
+    expect(fileContent).toBe(Customoptions.fileContent);
+  });
+
+  test("should return false if directory could not be created", () => {
+    const saveFile = new SaveFile();
+    const mkdirSpy = jest.spyOn(fs, "mkdirSync").mockImplementation(() => {
+      throw new Error("This is a custom testing error");
+    });
+    const result = saveFile.execute(Customoptions);
+
+    expect(result).toBe(false);
+    expect(mkdirSpy).toHaveBeenCalled();
+
+    // Restaurar
+    mkdirSpy.mockRestore();
+  });
+
+  test("should return false if file could not be created", () => {
+    const saveFile = new SaveFile();
+    const whriteFileSpy = jest
+      .spyOn(fs, "writeFileSync")
+      .mockImplementation(() => {
+        throw new Error("This is a custom testing error creating file");
+      });
+
+    const result = saveFile.execute({ fileContent: "Hola" });
+    expect(result).toBe(false);
+    whriteFileSpy.mockRestore();
   });
 });
